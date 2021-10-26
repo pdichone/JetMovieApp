18e62501cd0db6edf8a14143fc57c1e5510a317d
pdichone <pdichone@gmail.com>
2021-10-26 14:32:26 -0700

    Intro to LazyColumn

--- app/src/main/java/com/bawp/movieapp/MainActivity.kt
+++ app/src/main/java/com/bawp/movieapp/MainActivity.kt
@@ -3,36 +3,106 @@ package com.bawp.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bawp.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        Scaffold(topBar = {
+            TopAppBar(backgroundColor = Color.Magenta,
+                     elevation = 5.dp) {
+                 Text(text = "Movies")
+                
+            }
+        },) {
+            content()
+
+            
+        }
+         
+
+    }
+
+}
+@Composable
+fun MainContent(movieList: List<String> = listOf(
+    "Avatar",
+    "300",
+    "Harry Potter",
+    "Happiness...",
+    "Cross the Line...",
+    "Be Happy...",
+    "Happy Feet...",
+    "Life")) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+             items(items = movieList) {
+                    MovieRow(movie = it)
+             }
+        }
+
+    }
+
+}
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MovieRow(movie: String) {
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp) {
+        Row(verticalAlignment = Alignment.CenterVertically,
+           horizontalArrangement = Arrangement.Start) {
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp),
+                   shape = RectangleShape,
+                   elevation = 4.dp) {
+                Icon(imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image")
+
+            }
+            Text(text = movie)
+        }
+
+
+    }
+
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
-    }
+   MyApp {
+       MainContent()
+   }
 }
